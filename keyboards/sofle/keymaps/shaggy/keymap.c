7cebef0c058c5fbda343e08b093c6434f6c3cdf5
Yuval Kanar <kanaryuval@gmail.com>
2025-10-06 22:08:18 +0300

    Moved some keys around

--- keyboards/sofle/keymaps/shaggy/keymap.c
+++ keyboards/sofle/keymaps/shaggy/keymap.c
@@ -83,43 +83,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* LOWER
  * ,-----------------------------------------.                               ,-----------------------------------------.
- * | ____ |  F1  |  F2  |  F3  |  F4  |  F5  |                               |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |      |      |      |      |      |      |                               |      |      |      |      |   -  |   =  |
  * |------+------+------+------+------+------|                               |------+------+------+------+------+------|
- * | ____ |   1  |   2  |   3  |   4  |   5  |                               |   6  |   7  |   8  |   9  |   0  | F12  |
+ * | ____ |   1  |   2  |   3  |   4  |   5  |                               |   6  |   7  |   8  |   9  |   0  |   [  |
  * |------+------+------+------+------+------|                               |------+------+------+------+------+------|
- * | ____ |   !  |   @  |   #  |   $  |   %  |-------.               ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
+ * | ____ |   !  |   @  |   #  |   $  |   %  |-------.               ,-------|   ^  |   &  |   *  |   (  |   )  |   ]  |
  * |------+------+------+------+------+------| ____  |               | ____  |------+------+------+------+------+------|
- * | ____ |      |   _  |   -  |   =  |   {  |-------|               |-------|   }  |   [  |   ]  |   +  |      | ____ |
+ * | ____ |      |      |      |   _  |   {  |-------|               |-------|   }  |   +  |      |      |      | ____ |
  * `-------------+------+------+------+------|       |               |       |------+------+------+------+-------------'
  *               | ____ | ____ | ____ | ____ | ____  |               | ____  | ____ | ____ | ____ | ____ |
  *               `-----------------------------------'               '-----------------------------------'
  */
 
 [2] = LAYOUT(
-    _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                    KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
-    _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                    KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_F12 ,
-    _______,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                    KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PIPE,
-    _______,XXXXXXX,KC_UNDS,KC_MINS,KC_EQL ,KC_LCBR,_______,    _______,KC_RCBR,KC_LBRC,KC_RBRC,KC_PLUS,XXXXXXX,_______,
+    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_MINS,KC_EQL ,
+    _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                    KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_LBRC,
+    _______,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                    KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_RBRC,
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,KC_UNDS,KC_LCBR,_______,    _______,KC_RCBR,KC_PLUS,XXXXXXX,XXXXXXX,XXXXXXX,_______,
                     _______,_______,_______,_______,_______,    _______,_______,_______,_______,_______
 ),
 
 /* RAISE
  * ,----------------------------------------.                                ,-----------------------------------------.
- * |      |      |      |      |      |      |                               |      |      |      |      |      | Del  |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                               |  F6  |  F7  |  F8  |  F9  | F10  | Del  |
  * |------+------+------+------+------+------|                               |------+------+------+------+------+------|
- * | ____ | Ins  | Pscr | Menu |Pause |      |                               | PgUp | Home |  Up  | End  |      |      |
+ * | ____ | Ins  | Pscr | Menu |Pause |      |                               | PgUp | Home |  Up  | End  | F11  | F12  |
  * |------+------+------+------+------+------|                               |------+------+------+------+------+------|
  * | ____ | LCrl | LWin | LAlt |      |      |-------.               ,-------| PgDn | Left | Down | Rigth|      |      |
  * |------+------+------+------+------+------|       |               |       |------+------+------+------+------+------|
  * | ____ |      |      |      |      |      |-------|               |-------|      |      |      |      |      | ____ |
  * `-------------+------+------+------+------|       |               |       |------+------+------+------+-------------'
- *               | ____ | ____ | ____ |TG(3) | ____  |               | ____  | ____ | ____ | ____ | ____ |
+ *               | ____ | ____ | ____ |TG(1) | ____  |               | ____  | ____ | ____ | ____ | ____ |
  *               `-----------------------------------'               '-----------------------------------'
  */
 
 [3] = LAYOUT(
-    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_DEL ,
-    _______,KC_INS ,KC_PSCR,KC_APP ,KC_PAUS,XXXXXXX,                    KC_PGUP,KC_HOME,KC_UP  ,KC_END ,XXXXXXX,XXXXXXX,
+    XXXXXXX,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                    KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_DEL ,
+    _______,KC_INS ,KC_PSCR,KC_APP ,KC_PAUS,XXXXXXX,                    KC_PGUP,KC_HOME,KC_UP  ,KC_END ,KC_F11 ,KC_F12 ,
     _______,KC_LCTL,KC_LGUI,KC_LALT,XXXXXXX,XXXXXXX,                    KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,XXXXXXX,XXXXXXX,
     _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,
                     _______,_______,_______,TG(1)  ,_______,    _______,_______,_______,_______,_______
