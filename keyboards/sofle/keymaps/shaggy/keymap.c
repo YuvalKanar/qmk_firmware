1ca5b0869d98bf889f9da1141d797a64a7ba1cb6
Yuval Kanar <kanaryuval@gmail.com>
2026-01-10 12:30:18 +0200

    Add mac mode

--- keyboards/sofle/keymaps/shaggy/keymap.c
+++ keyboards/sofle/keymaps/shaggy/keymap.c
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                               |------+------+------+------+------+------|
  * | ____ | LCrl | LWin | LAlt |      |TG(GA)|-------.               ,-------| PgDn | Left | Down | Rigth|      |      |
  * |------+------+------+------+------+------|       |               |       |------+------+------+------+------+------|
- * | ____ |      |      |      |      |TG(GB)|-------|               |-------|      |      |      |      |      | ____ |
+ * | ____ |      |      |MACWIN|      |TG(GB)|-------|               |-------|      |      |      |      |      | ____ |
  * `-------------+------+------+------+------|       |               |       |------+------+------+------+-------------'
  *               | ____ | ____ | ____ | ____ | ____  |               | ____  | ____ | ____ | ____ | ____ |
  *               `-----------------------------------'               '-----------------------------------'
@@ -176,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                    KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_DEL ,
     _______,KC_INS ,KC_PSCR,KC_APP ,KC_PAUS,XXXXXXX,                    KC_PGUP,KC_HOME,KC_UP  ,KC_END ,KC_F11 ,KC_F12 ,
     _______,KC_LCTL,KC_LGUI,KC_LALT,XXXXXXX,TG(GA) ,                    KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,XXXXXXX,XXXXXXX,
-    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,TG(GB) ,XXXXXXX,    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,
+    _______,XXXXXXX,XXXXXXX,AG_TOGG,XXXXXXX,TG(GB) ,XXXXXXX,    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,
                     _______,_______,_______,_______,_______,    _______,_______,_______,_______,_______
 ),
 };
